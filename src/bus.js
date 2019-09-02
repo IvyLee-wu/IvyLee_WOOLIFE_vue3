62a03bc5711be3a84d5f42ae69f6b0754b30306a
IvyLee-wu <45351785+IvyLee-wu@users.noreply.github.com>
2019-09-02 14:30:04 +0800

    update

--- src/bus.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-Vue.prototype.$bus = new Vue();
-
-// Message
-// vm.$bus.$emit('message:push', message, status); 向外層傳遞
-// message(String): 訊息內容
-// status(String): Alert的樣式
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: '/IvyLee_WOOLIFE_vue3/',
-};
